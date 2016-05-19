85f3e4ff1236426265137eddab064e5d15281df4
lawrencegomez <lfg2305@gmail.com>
2016-05-18 23:41:59 -0700

    changed the font. Last commit of the night.

--- app.js
+++ app.js
@@ -12,46 +12,23 @@ var game = {x: 0,
             $player2Scoreboard: $('#player2Scoreboard'),
             $timer: $('#timer'),
             randBetween: function(a,b) {
-                    return Math.floor((Math.random()*b)+a);
+                    return Math.floor((Math.random()*b)+a); //Generates a random number between a and b
                     },
             svgEl: function(tagName) {
-                    return document.createElementNS("http://www.w3.org/2000/svg", tagName);
+                    return document.createElementNS("http://www.w3.org/2000/svg", tagName); //required code to use SVG elements
                     },
             $enemyMissile: $('#enemy-missile'),
             $playerMissile: $('player-missile'),
             timer: function() {
                       setInterval(function() {
-                      game.time ++
-                      game.$timer.text(': ' + game.time)
+                        game.time ++
+                        game.$timer.text(': ' + game.time)
                       }, 1000)
                     },
             }
 
-// var x = 0;
-// var time = 0;
-// var miss = 0;
-// var game = true;
-// var player1Score = 0;
-// var $board = $('#game-board');
-// var $start = $('#start');
-// var $player1Scoreboard = $('#player1Scoreboard');
-// var $palyer2Scoreboard = $('#player2Scoreboard');
-// var $timer = $('#timer')
-// var player1 = 'player1'
-// var player2 = 'player2'
-
-// function randBetween(a,b) {
-//   return Math.floor((Math.random()*b)+a);
-// }
-// function svgEl(tagName) {
-//    return document.createElementNS("http://www.w3.org/2000/svg", tagName);
-// }
-// mouseX = 0;
-// mouseY = 0;
-// var $enemyMissile = $('#enemy-missile');
-// var $playerMissile = $('#player-missile');
-
 // This initializes an explosion at the beginning so the rest of the code works
+// but it is not visible
 var explosionEl = game.svgEl("circle");
 $explosionEl = $(explosionEl).attr({
   cx: game.mouseX,
@@ -61,8 +38,10 @@ $explosionEl = $(explosionEl).attr({
   id: 'player-missile'
 });
 
+// set the current player to player 1
 game.currentPlayer = game.player1
 
+// function to switch turns
 function switchTurns() {
   if (game.currentPlayer == game.player1) {
     game.currentPlayer = game.player2;
@@ -71,6 +50,7 @@ function switchTurns() {
   }
 }
 
+// starts the game when the start button is clicked
 game.$start.on('click', function() {
   game.timer();
   console.log(game.currentPlayer)
@@ -79,7 +59,8 @@ game.$start.on('click', function() {
   }, game.randBetween(400, 1100))
 });
 
-
+// Function to grab the coordinates of where the mouse is clicked
+// and then generate an explosion on those coordinates
 game.$board.on('click',  function(event) {
   game.mouseX = event.clientX - 515;
   game.mouseY = event.clientY - 350;
@@ -92,15 +73,15 @@ game.$board.on('click',  function(event) {
     fill:"white",
     id: 'player-missile'
   });
-  game.$board.append($explosionEl)
-  // var $playerR = $playerMissile.attr('r');
+  game.$board.append($explosionEl) //appends the epxlosion to the game board
+//animates the explosion by enlarging the radius, using the random num generator
   $explosionEl.animate({r:game.randBetween(30,40)},
       {
-          duration: 400,
+          duration: 400, //duration of the animation in milliseconds
           step: function(now) { $(this).attr("r", now);},
+          //when the animation is done, perform the following actions
           done: function() {
             $(this).remove()
-            // destroyed = false
             $(this).attr({
               cy: 0,
               cx: 1500,
@@ -112,10 +93,10 @@ game.$board.on('click',  function(event) {
 
 // constructor for a new missile
 function Missile() {
-  game.x = game.randBetween(20, 1100);
+  game.x = game.randBetween(20, 1100); //randomizes the x value of where the missile will fall from
   var missileEl = game.svgEl('circle')
   $missileEl = $(missileEl)
-  $missileEl.attr({
+  $missileEl.attr({ //the attributes of each new missile
     cx: game.x,
     cy: -25,
     r: 5,
@@ -123,13 +104,16 @@ function Missile() {
     fill: 'red',
     id: 'enemy-missile'
   })
-  game.$board.append($missileEl);
+  game.$board.append($missileEl); //appends a new missile to the game board
   // var $enemyR = $enemyMissile.attr('r');
-  $missileEl.animate({ cy: 800 },
+  $missileEl.animate({ cy: 800 }, //animate the cy value of the missile
       {
           duration: 10000,
           step: function(now) {
              $(this).attr("cy", now);
+             //the below code checks for a collision by looking at the distance any missile
+             //is from an explosion and then compares the radius' to see if they actually collided
+             //and if they did collide, it removes that element from the board
              var dy = (Number($explosionEl.attr("cy")) - Number($(this).attr("cy")))
              var dx = (Number($explosionEl.attr("cx")) - Number($(this).attr("cx")))
              var distance = Math.sqrt((dx*dx) + (dy*dy))
@@ -137,18 +121,12 @@ function Missile() {
                  $(this).remove()
              }
             },
-          done: function() {
+          done: function() { //when the animation is done, remove the falling missile
             $(this).remove()
         }
       });
     }
 
-// function timer() {
-//   setInterval(function() {
-//     time ++
-//     $timer.text(': ' + time)
-//   }, 1000)
-// }
 
 // function stopAnimation() {
 //     $missileEl.stop();
